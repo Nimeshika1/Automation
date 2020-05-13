3af061c290eef572f43d3c47d3d8c4f64ed9d2f8
Nimeshika1 <65106206+Nimeshika1@users.noreply.github.com>
2020-05-13 06:44:53 +0530

    Add files via upload

--- src/main/java/com/sysco/automation_web_UI/page/AdidasWomanPage.java
+++ src/main/java/com/sysco/automation_web_UI/page/AdidasWomanPage.java
@@ -5,9 +5,19 @@ import org.openqa.selenium.By;
 public class AdidasWomanPage extends HomePage{
 
     protected By btnProduct1 = By.xpath("//*[@class=\"product-image-wrapper\"]/div/img");
+    protected By lblPrice = By.xpath("//*[@class=\"product-item-link\"]");
+    protected By lblName = By.xpath("//*[@class=\"price\"]");
 
     public void clickProductOne(){
         syscoLabUI.waitTillElementLoaded(btnProduct1);
         syscoLabUI.click(btnProduct1);
+    }
+
+    public String getPrice(){
+        return syscoLabUI.getText(lblPrice);
+    }
+
+    public String getName(){
+        return syscoLabUI.getText(lblName);
     }
 }
